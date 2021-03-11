fc66fdf4b0d3505ec411846f59d1e2431daf4ab7
dimav47 <dimav47@yandex.ru>
2021-03-11 17:53:26 +0300

    3 commit

--- goole.tf
+++ goole.tf
@@ -1,11 +1,12 @@
 provider "google" {
- credentials = file("graphic-centaur-302312-9c92a9be897b.json")
- project = "{{graphic-centaur-302312}}"
+  credentials = file("Project-450fbf6389c5.json")
+  project = "graphic-centaur-302312"
   region  = "us-central1"
   zone    = "us-central1-a"
+  user_project_override = true
 }
 
-resource "google_compute_instance" "vm_instance"  {
+resource "google_compute_instance" "vm-instance"  {
   metadata = {
     ssh-keys = "BUH-d072:${file("~/.ssh/id_rsa.pub")}"
     }
@@ -13,7 +14,9 @@ resource "google_compute_instance" "vm_instance"  {
   machine_type = "e2-small"
   boot_disk {
     initialize_params {
-      image = "Ubuntu 20.04"
+      size = "15"
+      type = "pd-balanced"
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
   }
  network_interface {
